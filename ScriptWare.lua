501b66966aa6184d8dd38db732608fdb3bfc438d
Wh3L1nuxXaa <137390554+L1nuxX@users.noreply.github.com>
2024-05-17 20:50:42 +0300

    Update ScriptWare.lua

--- ScriptWare.lua
+++ ScriptWare.lua
@@ -1,3 +1,5 @@
+print("hooked ScriptWare")
+
 local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/L1nuxX/ScriptWare/main/UILib.lua'))()
 
 local window = lib.createWindow("ScriptWare", "ScriptWare", true)
@@ -9,4 +11,8 @@ local Other = window.createTab("Other")
 Other.createButton("unhook", function()
 	print("Unhoocked ScriptWare!")
   window.deleteWindow("window")
+end)
+
+MD2.createSlider("SpeedHack", {defualt = 10, max = 64, min = 1}, function(speedhackvalue)
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speedhackvalue
 end)
