fccd406e181c6a6b9ab5c57064ab2f6c0d8ed2e9
Wh3L1nuxXaa <137390554+L1nuxX@users.noreply.github.com>
2024-05-17 20:35:31 +0300

    Update ScriptWare.lua

--- ScriptWare.lua
+++ ScriptWare.lua
@@ -1,3 +1,3 @@
-local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/L1nuxX/Viseux/main/AquaLib.lua'))()
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/L1nuxX/ScriptWare/main/UILib.lua'))()
 
 local window = lib.createWindow("Viseux", "Viseux", true)
