553d7a63a4e5fc0968e55d2edba77901563ffeef
Wh3L1nuxXaa <137390554+L1nuxX@users.noreply.github.com>
2024-05-17 20:41:21 +0300

    Update ScriptWare.lua

--- ScriptWare.lua
+++ ScriptWare.lua
@@ -1,3 +1,7 @@
 local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/L1nuxX/ScriptWare/main/UILib.lua'))()
 
 local window = lib.createWindow("ScriptWare", "ScriptWare", true)
+
+local Combat = window.createTab("MurderMustery2")
+local Combat = window.createTab("Ibbys")
+local Combat = window.createTab("Other")
