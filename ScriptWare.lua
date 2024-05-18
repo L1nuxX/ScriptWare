dbf6dc3d974cb83905c4db1f8078bc9f73ece226
Wh3L1nuxXaa <137390554+L1nuxX@users.noreply.github.com>
2024-05-18 10:01:05 +0300

    Update ScriptWare.lua

--- ScriptWare.lua
+++ ScriptWare.lua
@@ -15,20 +15,9 @@ Other.createButton("unhook", function()
   window.deleteWindow("window")
 end)
 
--- Переменная для хранения значения скорости
-local speedhackvalue = 18
-
 MD.createSlider("SpeedHack", {defualt = 18, max = 64, min = 16}, function(value)
-	speedhackvalue = value
-end)
-
--- Цикл для постоянного обновления WalkSpeed
-spawn(function()
 	while true do
-		-- Проверка на существование игрока и его персонажа
-		if game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
-			game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speedhackvalue
-		end
-		wait(0.1) -- Задержка в 0.1 секунды
-	end
+    print("Этот цикл будет выполняться бесконечно")
+    -- Добавьте сюда другие действия, которые должны выполняться бесконечно
+end
 end)
